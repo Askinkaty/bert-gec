f53771bccdc10ab5f9513ec632f6930ce69de562
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2022-05-01 11:36:28 +0300

    Mods in run scrips

--- scripts/generate_ru.sh
+++ scripts/generate_ru.sh
@@ -16,15 +16,9 @@ PROCESSED_DIR=$DATA_DIR/process
 MODEL_DIR=$BASE_DIR/gec_model
 
 
-train_src=$DATA_DIR/train.src
-train_trg=$DATA_DIR/train.trg
-valid_src=$DATA_DIR/valid.src
-valid_trg=$DATA_DIR/valid.trg
-test_src=$DATA_DIR/test.src
-test_trg=$DATA_DIR/test.trg
 
-cpu_num=`grep -c ^processor /proc/cpuinfo`
-
+python -u detok.py $input $PROCESSED_DIR/test.bert.src
+paste -d "\n" $PROCESSED_DIR/test.src $PROCESSED_DIR/test.bert.src > $PROCESSED_DIR/test.cat.src
 
 echo Generating...
 CUDA_VISIBLE_DEVICES=$gpu python -u ${FAIRSEQ_DIR}/interactive.py $PREPROCESS \
@@ -39,7 +33,7 @@ CUDA_VISIBLE_DEVICES=$gpu python -u ${FAIRSEQ_DIR}/interactive.py $PREPROCESS \
     --log-format simple \
     --remove-bpe \
     --bert-model-name $bert_type \
-    < $MODEL_DIR/test.cat.src > $MODEL_DIR/test.nbest.tok
+    < $PROCESSED_DIR/test.cat.src > $PROCESSED_DIR/test.nbest.tok
 
-cat $MODEL_DIR/test.nbest.tok | grep "^H"  | python -c "import sys; x = sys.stdin.readlines(); x = ' '.join([ x[i] for i in range(len(x)) if (i % ${beam} == 0) ]); print(x)" | cut -f3 > $MODEL_DIR/test.best.tok
-sed -i '$d' $MODEL_DIR/test.best.tok
+cat $PROCESSED_DIR/test.nbest.tok | grep "^H"  | python -c "import sys; x = sys.stdin.readlines(); x = ' '.join([ x[i] for i in range(len(x)) if (i % ${beam} == 0) ]); print(x)" | cut -f3 > $PROCESSED_DIR/test.best.tok
+sed -i '$d' $PROCESSED_DIR/test.best.tok
