ee902919cd48e7133f8c08996d86179cedca9b4c
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2022-05-30 18:37:09 +0300

    Mods in run scrips

--- scripts/train_ru.sh
+++ scripts/train_ru.sh
@@ -60,7 +60,7 @@ cpu_num=`grep -c ^processor /proc/cpuinfo`
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -u $FAIRSEQ_DIR/train.py $PROCESSED_DIR/bin \
     --save-dir $MODEL_DIR \
     --arch transformer_vaswani_wmt_en_de_big \
-    --max-tokens 4096 \
+    --max-tokens 10000 \
     --optimizer adam \
     --lr 0.00003 \
     -s src \
